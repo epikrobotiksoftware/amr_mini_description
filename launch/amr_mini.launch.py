c7321248185e6cf59d98c2a0e16b28c079319e3a
MehmetKUTAN <mehmettkutann.com>
2023-02-28 14:23:42 +0300

    amr_mini

--- launch/amr_mini.launch.py
+++ launch/amr_mini.launch.py
@@ -13,25 +13,15 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     #############################################################
     use_sim_time = os.environ.get('USE_SIM_TIME', True)
+
+    pkg_dir = get_package_share_directory('amr_mini_description')
+
     xacro_file_name = os.environ.get('XACRO_FILE_NAME', 'amr_mini.xacro')
-
-    print("xacro_file_name : {}".format(xacro_file_name))
-    #############################################################
-
     xacro_urdf_path = os.path.join(get_package_share_directory(
         'amr_mini_description'), 'urdf', xacro_file_name)
     robot_description_config = xacro.process_file(xacro_urdf_path)
     robot_desc = robot_description_config.toxml()
-
-    robot_localization_file_path = os.path.join(get_package_share_directory(
-        'amr_mini_description'), 'config/ekf.yaml')
-
-    pkg_dir = get_package_share_directory('amr_mini_description')
     #############################################################
-
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            pkg_dir + '/launch/gazebo.launch.py'))
 
     robot_state_publisher = Node(package='robot_state_publisher',
                                  executable='robot_state_publisher',
@@ -45,34 +35,28 @@ def generate_launch_description():
                                      {'use_sim_time': use_sim_time}],
                                  )
 
-    rviz_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            pkg_dir + '/launch/rviz.launch.py'))
-
-    robot_localization_node = Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[robot_localization_file_path,
-                    {'use_sim_time': use_sim_time}]
-    )
-
-    merge_laser_scan = Node(
-        package='amr_mini_description',
-        executable='merge_laser_scan',
-        name='merge_laser_scan',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments={'front_lidar_amr_mini_laser', 'back_lidar_amr_mini_laser'}
-    )
-
     # rqt_robot_steering = Node(package='rqt_robot_steering',
     #                           executable='rqt_robot_steering',
     #                           name='rqt_robot_steering',
     #                           output='screen',
     #                           #   parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}]
     #                           )
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            pkg_dir + '/launch/gazebo.launch.py'))
+
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            pkg_dir + '/launch/rviz.launch.py'))
+
+    robot_localization_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            pkg_dir + '/launch/robot_localization.launch.py'))
+
+    merge_laser_scan_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            pkg_dir + '/launch/merge_lidar.launch.py'))
 
     mapping_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -89,9 +73,9 @@ def generate_launch_description():
         gazebo_launch,
         joint_state_publisher,
         robot_state_publisher,
-        robot_localization_node,
+        robot_localization_launch,
         # rqt_robot_steering,
-        merge_laser_scan,
+        merge_laser_scan_launch,
         rviz_launch,
         # mapping_launch,
         amcl_launch,
