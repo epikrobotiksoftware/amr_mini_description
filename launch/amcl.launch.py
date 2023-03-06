e6710a71ef656fda37e4ce5f0fffc326dc8360af
MehmetKUTAN <mehmettkutann.com>
2023-03-06 14:05:54 +0300

    Amr_mini

--- launch/amcl.launch.py
+++ launch/amcl.launch.py
@@ -7,8 +7,6 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-# dsadasdasdsadsadasd
-
 
 def generate_launch_description():
 
