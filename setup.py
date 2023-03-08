06d054e4ea27ab75e4316ab93d2a342941e427d5
MehmetKUTAN <mehmettkutann.com>
2023-03-08 17:27:04 +0300

    Amr_mini

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name+"/"+package_name), glob(package_name+'/*')),
+        (os.path.join('share', package_name+"/" +
+         package_name), glob(package_name+'/*.py')),
         (os.path.join('share', package_name+'/launch'), glob('launch/*.py')),
         (os.path.join('share', package_name+'/rviz'), glob('rviz/*')),
         (os.path.join('share', package_name+'/urdf'),
